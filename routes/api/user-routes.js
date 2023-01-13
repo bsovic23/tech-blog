104d70b704d8e69ddd1fed273d5c0b57e2fc2fa1
Brit Sovic <bsovic@tulane.edu>
2023-01-13 11:57:03 -0500

    update user api and routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Get /api/users
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Get /api/users/:id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no user with this Id' });
+            return;
+        } 
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err);
+    });
+});
+
+// Post /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Put /api/users/:id
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no user with this id '})
+            return;
+        } res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err);
+    });
+});
+
+// Delete /api/users/:id
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
